2e27812d2671be048f965dc5119427eb242d30e0
joekhiu <33024182+joekhiu@users.noreply.github.com>
2019-10-12 17:17:32 +0800

    various commands using Ansible

---
+++ rpi_showresolv.sh
@@ -0,0 +1,7 @@
+# Script used with Ansible for running commands remotely
+# rpi_showresolv.sh
+#
+# Section 6 Ansible Modules, Lesson 18 - Ansible Modules
+#
+# last updated: 12-Oct-2019
+cat /etc/resolv.conf >> /home/pi/Desktop/rpi_showresolv.txt
